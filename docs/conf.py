719c1ff5aa85fd4a5932370e4c19001da83a046b
Tuck Williamson <williamsonth@g.cofc.edu>
2025-10-01 00:45:58 -0400

    Bump version: 0.2.0 → 0.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ project = "agh"
 year = "2025"
 author = "Tuck Williamson"
 copyright = f"{year}, {author}"
-version = release = "0.2.0"
+version = release = "0.2.1"
 
 pygments_style = "trac"
 templates_path = ["."]
