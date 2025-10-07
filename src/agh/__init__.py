64d819be7e34cbc8ce052f4467f85eefcd3996b0
Tuck Williamson <williamsonth@g.cofc.edu>
2025-10-07 13:57:12 -0400

    Fix for Dist:check.

--- src/agh/__init__.py
+++ src/agh/__init__.py
@@ -1,15 +1,9 @@
 from .agh_data import Assignment
+from .agh_data import OutputSectionData
 from .agh_data import Submission
+from .agh_data import SubmissionFileData
 from .core import start
 
 __version__ = "0.2.5"
 
-from .agh_data import Assignment, Submission, SubmissionFileData, OutputSectionData
-
-__all__ = [
-    "__version__",
-    "Assignment",
-    "Submission",
-    "SubmissionFileData",
-    "OutputSectionData",
-]
+__all__ = ["Assignment", "OutputSectionData", "Submission", "SubmissionFileData", "__version__", "start"]
